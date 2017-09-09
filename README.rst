e6a0a8042652d04a5d8e10926a8d547f447deb91
msimonin <matthieu.simonin@inria.fr>
2017-09-09 10:25:34 +0200

    PO~

--- README.rst
+++ README.rst
@@ -26,117 +26,17 @@
             {"type": "kavlan-global", "role": "network3", "site": "lille"}]
     }
 
-    # Follow this workflow :
-    #
-    # It's idempotent and stateless
-    #
-    # resources will be decorated with some extra information : the _c_* keys
-    c_resources = reserve(resources, job_name="test", walltime="01:00:00")
-    c_resources = deploy(c_resources)
-    c_resources = configure_network(c_resources)
-
-    # And you'll end up with : 
-    print(json.dumps(c_resources, indent=4))
-    """
-    {
-        "networks": [
-            {
-                "role": "network1",
-                "type": "prod",
-                "site": "nancy",
-                "_c_network": {
-                    "site": "nancy",
-                    "gateway": "172.16.79.254",
-                    "vlan_id": null,
-                    "network": "172.16.64.0/20"
-                }
-            },
-            {
-                "role": "network2",
-                "type": "kavlan",
-                "site": "nancy",
-                "_c_network": {
-                    "site": "nancy",
-                    "gateway": "10.16.63.254",
-                    "vlan_id": 4,
-                    "network": "10.16.0.0/18"
-                }
-            },
-            {
-                "role": "network3",
-                "type": "kavlan-global",
-                "site": "lille",
-                "_c_network": {
-                    "site": "lille",
-                    "gateway": "10.11.255.254",
-                    "vlan_id": 12,
-                    "network": "10.11.192.0/18"
-                }
-            }
-        ],
-        "machines": [
-            {
-                "_c_nics": [
-                    [
-                        "eth0",
-                        "network1"
-                    ],
-                    [
-                        "eth1",
-                        "network2"
-                    ],
-                    [
-                        "eth2",
-                        "network3"
-                    ]
-                ],
-                "primary_network": "network1",
-                "_c_deployed": [
-                    "grisou-40.nancy.grid5000.fr"
-                ],
-                "cluster": "grisou",
-                "role": "compute",
-                "_c_nodes": [
-                    "grisou-40.nancy.grid5000.fr"
-                ],
-                "nodes": 1,
-                "_c_undeployed": [],
-                "secondary_networks": [
-                    "network2",
-                    "network3"
-                ]
-            },
-            {
-                "_c_nics": [
-                    [
-                        "eth0",
-                        "network1"
-                    ],
-                    [
-                        "eth1",
-                        "network2"
-                    ],
-                    [
-                        "eth2",
-                        "network3"
-                    ]
-                ],
-                "primary_network": "network1",
-                "_c_deployed": [
-                    "grisou-43.nancy.grid5000.fr"
-                ],
-                "cluster": "grisou",
-                "role": "control",
-                "_c_nodes": [
-                    "grisou-43.nancy.grid5000.fr"
-                ],
-                "nodes": 1,
-                "_c_undeployed": [],
-                "secondary_networks": [
-                    "network2",
-                    "network3"
-                ]
-            }
-        ]
+    r = Resources(resources)
+    options = {
+      "job_name": "test",
+      "walltime": "01:00:00",
     }
-    """
+    # This
+    r.reserve(**options)
+    r.deploy(**options)
+    r.configure_network(**options)
+    # Or this
+    # r.launch(**options)
+
+    r.get_roles()
+    r.get_networks()
--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -1,5 +1,6 @@
-from deploy5k import api
+from deploy5k.api import Resources, ENV_NAME
 from deploy5k import utils
+from deploy5k import schema
 from deploy5k.schema import PROD, KAVLAN
 import mock
 import unittest
@@ -8,57 +9,62 @@ class TestGetNetwork(unittest.TestCase):
 
     def test_no_concrete_network_yet(self):
         expected_networks = [{"type": PROD, "role": "network1"}]
-        resources = {"networks": expected_networks}
-        networks = api.get_networks(resources)
+        schema.validate = mock.Mock()
+        r = Resources({"networks": expected_networks})
+        networks = r.get_networks()
         self.assertItemsEqual(expected_networks, networks)
 
     def test_concrete_network(self):
         networks = [{"type": KAVLAN, "role": "network1", "_c_network": {"site": "nancy", "vlan_id": 1}}]
         expected_networks = [{"type": KAVLAN, "role": "network1", "site": "nancy", "vlan_id": 1}]
-        resources = {"networks": networks}
-        networks = api.get_networks(resources)
+        schema.validate = mock.Mock()
+        r = Resources({"networks": networks})
+        networks = r.get_networks()
         self.assertItemsEqual(expected_networks, networks)
 
 class TestDeploy(unittest.TestCase):
 
     def test_prod(self):
         nodes = ["foocluster-1", "foocluster-2"]
-        c_resources = {
+        schema.validate = mock.Mock()
+        r = Resources({
             "machines": [{
                 "_c_nodes": nodes,
                 "primary_network": "network1"
             }],
             "networks": [{"type": PROD, "role": "network1"}]
-        }
+        })
         deployed = set(["foocluster-1", "foocluster-2"])
         undeployed = set()
         utils._deploy = mock.Mock(return_value=(deployed, undeployed))
-        c_resources = api.deploy(c_resources)
-        utils._deploy.assert_called_with(nodes, False, {"env_name": api.ENV_NAME})
-        self.assertItemsEqual(deployed, c_resources["machines"][0]["_c_deployed"])
-        self.assertItemsEqual(undeployed, c_resources["machines"][0]["_c_undeployed"])
+        r.deploy()
+        utils._deploy.assert_called_with(nodes, False, {"env_name": ENV_NAME})
+        self.assertItemsEqual(deployed, r.c_resources["machines"][0]["_c_deployed"])
+        self.assertItemsEqual(undeployed, r.c_resources["machines"][0]["_c_undeployed"])
 
     def test_vlan(self):
         nodes = ["foocluster-1", "foocluster-2"]
-        c_resources = {
+        schema.validate = mock.Mock()
+        r = Resources({
             "machines": [{
                 "_c_nodes": nodes,
                 "primary_network": "network1"
             }],
             "networks": [{"type": KAVLAN, "role": "network1", "_c_network": {"site": "rennes", "vlan_id": 4}}]
-        }
+        })
         deployed = set(["foocluster-1", "foocluster-2"])
         undeployed = set()
         utils._deploy = mock.Mock(return_value=(deployed, undeployed))
-        c_resources = api.deploy(c_resources)
-        utils._deploy.assert_called_with(nodes, False, {"env_name": api.ENV_NAME, "vlan": 4})
-        self.assertItemsEqual(deployed, c_resources["machines"][0]["_c_deployed"])
-        self.assertItemsEqual(undeployed, c_resources["machines"][0]["_c_undeployed"])
+        r.deploy()
+        utils._deploy.assert_called_with(nodes, False, {"env_name": ENV_NAME, "vlan": 4})
+        self.assertItemsEqual(deployed, r.c_resources["machines"][0]["_c_deployed"])
+        self.assertItemsEqual(undeployed, r.c_resources["machines"][0]["_c_undeployed"])
 
     def test_2_deployements_with_undeployed(self):
         nodes_foo = ["foocluster-1", "foocluster-2"]
         nodes_bar = ["barcluster-1", "barcluster-2"]
-        c_resources = {
+        schema.validate = mock.Mock()
+        r = Resources({
             "machines": [{
                 "_c_nodes": nodes_foo,
                 "primary_network": "network1"
@@ -70,17 +76,17 @@ class TestDeploy(unittest.TestCase):
             "networks": [
                 {"type": PROD, "role": "network1"},
                 {"type": KAVLAN, "role": "network2", "_c_network": {"site": "rennes", "vlan_id": 4}}]
-        }
+        })
         d_foo = set(["foocluster-1"])
         u_foo = set(nodes_foo) - d_foo
         d_bar = set(["barcluster-2"])
         u_bar = set(nodes_bar) - d_bar
         utils._deploy = mock.Mock(side_effect=[(d_foo, u_foo), (d_bar, u_bar)])
-        c_resources = api.deploy(c_resources)
+        r.deploy()
         self.assertEquals(2, utils._deploy.call_count)
-        self.assertItemsEqual(d_foo, c_resources["machines"][0]["_c_deployed"])
-        self.assertItemsEqual(u_foo, c_resources["machines"][0]["_c_undeployed"])
-        self.assertItemsEqual(d_bar, c_resources["machines"][1]["_c_deployed"])
-        self.assertItemsEqual(u_bar, c_resources["machines"][1]["_c_undeployed"])
+        self.assertItemsEqual(d_foo, r.c_resources["machines"][0]["_c_deployed"])
+        self.assertItemsEqual(u_foo, r.c_resources["machines"][0]["_c_undeployed"])
+        self.assertItemsEqual(d_bar, r.c_resources["machines"][1]["_c_deployed"])
+        self.assertItemsEqual(u_bar, r.c_resources["machines"][1]["_c_undeployed"])
 
 
