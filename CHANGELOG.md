bda8c861e462b5e31bb6ecc7892fae2f789956f2
msimonin <matthieu.simonin@inria.fr>
2017-09-11 09:53:06 +0200

    add changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# 0.0.2 (current)
+
+	* todo
+
+# 0.0.1
+
+* Initial version
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='deploy5k',
-    version='0.0.1',
+    version='0.0.2',
     description='',
     url='',
     author='msimonin',
